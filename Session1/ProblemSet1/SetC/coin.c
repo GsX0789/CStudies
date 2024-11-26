93f41a5708a8ba0b7b40ce4defd48e8c00e610ba
Guilherme <156434433+GsX0789@users.noreply.github.com>
2024-11-25 23:04:40 -0300

    Update coin.c
    Fixing some grammatical errors

--- Session1/ProblemSet1/SetC/coin.c
+++ Session1/ProblemSet1/SetC/coin.c
@@ -19,6 +19,6 @@ void GreedyMachine(int coin_amount)
     const int NICKEL_COIN = 5;
     const int PENNY_COIN = 1;
 
-    //See wheter four while loops is necessary, or if it can be done with one while loop and for if's
+    //See whether four while loops is necessary, or if it can be done with one while loop and for if's
     //Or One While loop and one switch statment!
 }
