d6b9313bb1c1cac318ba09973c2a6fdb80212398
GsX07 <zreflect12@gmail.com>
2024-11-25 23:10:14 -0300

    Exercise 03(coin.c)

---
+++ Session1/ProblemSet1/SetC/coin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//In this exercise, the user will pass a coin amount, let's say 85c cents, and we need to use
+//the greedy algorithm to evaluate how many coins is need to a given amount!
+//Example:
+//
+
+int main(void)
+{
+
+
+    return 0;
+}
+
+void GreedyMachine(int coin_amount)
+{
+    const int QUARTER_COIN = 25;
+    const int DIME_COIN = 10;
+    const int NICKEL_COIN = 5;
+    const int PENNY_COIN = 1;
+
+    //See wheter four while loops is necessary, or if it can be done with one while loop and for if's
+    //Or One While loop and one switch statment!
+}
