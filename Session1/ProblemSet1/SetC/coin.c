2726cd8de9e8eb778b511f7581f2c758cdd08834
GsX07 <zreflect12@gmail.com>
2024-11-26 23:06:24 -0300

    Coin.c is a coin charge program, the user will give a x amount of coins and we give them back the amount of coins needed to reach that amount

--- Session1/ProblemSet1/SetC/coin.c
+++ Session1/ProblemSet1/SetC/coin.c
@@ -5,20 +5,61 @@
 //Example:
 //
 
+void GreedyMachine(int);
+
 int main(void)
 {
+    int coin_change = 0;
+    printf("Coins charge, insert a value, 100c or lower\n");
 
+    //Do a verification, to notice if the user input is inside the boundaries!
+    do
+    {
+        printf(">");
+        scanf("%i", &coin_change);
+        
+    } while (coin_change > 100 || coin_change < 0);
+
+    GreedyMachine(coin_change);
 
     return 0;
 }
 
 void GreedyMachine(int coin_amount)
 {
+    //const to make it easier to subtract;
     const int QUARTER_COIN = 25;
     const int DIME_COIN = 10;
     const int NICKEL_COIN = 5;
     const int PENNY_COIN = 1;
 
-    //See wheter four while loops is necessary, or if it can be done with one while loop and for if's
-    //Or One While loop and one switch statment!
+    int coins_quantity = 0;
+    
+
+    while(coin_amount > 0)
+    {
+        //Here is where the Greedy algorithm comes in!
+        if(coin_amount >= QUARTER_COIN)
+        {
+            coin_amount -= QUARTER_COIN;
+            coins_quantity++;
+        }
+        else if(coin_amount >= DIME_COIN && coin_amount <=QUARTER_COIN)
+        {
+            coin_amount -= DIME_COIN;
+            coins_quantity++;
+        }
+        else if(coin_amount >= NICKEL_COIN && coin_amount < DIME_COIN)
+        {
+            coin_amount -= NICKEL_COIN;
+            coins_quantity++;
+        }
+        else if(coin_amount >= PENNY_COIN && coin_amount < NICKEL_COIN)
+        {
+            coin_amount -= PENNY_COIN;
+            coins_quantity++;
+        }
+    }
+
+    printf("Coin(s) amount: %i", coins_quantity);
 }
