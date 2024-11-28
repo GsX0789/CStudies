557f11f0b650ac72e50f39af4c9e15d7460d4121
GsX07 <zreflect12@gmail.com>
2024-11-27 23:11:16 -0300

    Now the program asks the user a message and then encrypt it

--- Session2/MessingWithArrray/funWArrays.c
+++ Session2/MessingWithArrray/funWArrays.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-//This program encrypts Hello
+//Asks the user a simple word to encrypt
 
 int main(void)
 {
 
-    const int KEY_ADDER = 15;
+    const int KEY_ADDER = 9;
 
-    char fixed_message[] = "Hello";
+    char fixed_message[10];
+    printf("Inserts a short word max 10 letters to encrypt\n");
+    printf(">");
+    scanf("%s", &fixed_message);
 
+    
     printf("\nMessage before: %s", fixed_message);
 
     for (int index = 0; index < strlen(fixed_message); index++)
